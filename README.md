e6d6534dc5c988ed31822e8456e3438927f797f5
MSantos023 <santosgm023@gmail.com>
2025-09-26 13:15:03 +0200

    Primera nocion muy basica de pyhton

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cursos de practica de python impartido 
+# Primera Clase:
+- Hola Mundo 
+- Tipos de datos
+- Conversion de tipos
+- Variables
+- Numbers 
+- Booleans
+- Strings
+- Ejercicios
---
+++ basic/01_basic.py
@@ -0,0 +1,11 @@
+###
+# 01_basic print()
+# print() es la forma que nos permite imprimir por consola.
+###
+
+print("Hola mundo, este es mi primer print en python.")
+print('En python tambien se puede utilizar las comillas simple.')
+print("tambien se puede pasar", "distintos", "parametros y los separa por espacios en blanco")
+print("tambien se les puede","decir que separador queremos", "que use para separar los parametros", sep="-")
+print("otro parametro que se puede cambiar es el final", "si no quieres que por ejemplo haga un salto de linea puedes decirselo", end=" ")
+print("Obviamente tambien hacepta numeros y mas tipos de datos", 1412341234)
